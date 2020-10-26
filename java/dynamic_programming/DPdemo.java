b8429e3fc1c80d75e5d1f3d4a8a36afeb01ab7da
ayanagarwal2000 <56232928+ayanagarwal2000@users.noreply.github.com>
2020-10-26 15:08:30 +0530

    Update DPdemo.java

--- java/dynamic_programming/DPdemo.java
+++ java/dynamic_programming/DPdemo.java
@@ -1,1 +1,20 @@
-public class DPdemo{}
+public class DPdemo{
+	public static void main (String[] arg){
+		System.out.println(fib(9));
+		
+	}
+	
+	public static int fib(int n){
+		if(n==0){
+			return n;
+		}
+		if(n==1){
+			return n;
+		}
+		int fnm1=fib(n-1);
+		int fnm2=fib(n-2);
+		int fnm=fnm1+fnm2;
+		
+		return fnm;
+	}
+}
