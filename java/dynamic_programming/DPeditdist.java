180bf1669c2f4c0aaf36a59f732bc0129521d6b6
Ayan Agarwal <56232928+ayanagarwal2000@users.noreply.github.com>
2020-11-23 17:07:18 +0530

    Create DPeditdist.java

---
+++ java/dynamic_programming/DPeditdist.java
@@ -0,0 +1,18 @@
+public class DPeditdist{
+	public static long start;
+	public static long end;
+	
+	public static void startAlgo(){
+		start=System.currentTimeMillis();
+		//return start;
+	}
+	public static long endAlgo(){
+		end=System.currentTimeMillis();
+		return end-start;
+	}
+	
+	public static void main(String[] arg){
+		
+	}
+	
+}
