16436c795e012ea6c4632456d89fc86518a4b077
ayanagarwal2000 <56232928+ayanagarwal2000@users.noreply.github.com>
2020-11-29 18:21:38 +0530

    Update graph.java

--- java/graphs/graph.java
+++ java/graphs/graph.java
@@ -1,2 +1,13 @@
+import java.util.HashMap;
+
 public class graph{
+	private class Vertex{
+		HashMap<String, Integer> nbrs=new HashMap<>();
+	}
+	
+	HashMap<String, Vertex> vtces;
+	
+	public graph(){
+		vtces=new HashMap<>();
+	}
 }
