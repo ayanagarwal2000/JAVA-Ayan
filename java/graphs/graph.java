81ca2a1b85a549b6d434152e9ec00f3d91744c2c
ayanagarwal2000 <56232928+ayanagarwal2000@users.noreply.github.com>
2020-11-29 19:33:09 +0530

    Update graph.java

--- java/graphs/graph.java
+++ java/graphs/graph.java
@@ -1,5 +1,6 @@
 import java.util.HashMap;
-
+import java.io.*; 
+import java.util.*; 
 public class graph{
 	private class Vertex{
 		HashMap<String, Integer> nbrs=new HashMap<>();
@@ -23,5 +24,31 @@ public class graph{
 		Vertex vtc=new Vertex();
 		vtces.put(vname, vtc);
 	}
+	
+	public int numEdges(){
+		ArrayList<String> keys=new ArrayList<>(vtces.keySet());
+		int count=0;
+		for(String key :keys){
+			Vertex vtc=vtces.get(key);
+			int size=vtc.nbrs.size();
+			count=count+size;
+		}
+		return count/2;
+	}
+	
+	public boolean containsEdge(String vname1, String vname2){
+		Vertex vtc=vtces.get(vname1);
+		/*ArrayList<String> keys=new ArrayList<>(vtc.nbrs.keySet());
+		for(String key :keys){
+		    if(key==vname2){
+				return true;
+			}
+		}*/
+		if(vtc.nbrs.containsKey(vname2)){
+			return true;
+		}
+		return false;
+	}
+	
 	
 }
