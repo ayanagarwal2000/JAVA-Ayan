4761598ae2feb512816ea153a90c31ee5c3b883b
Ayan Agarwal <56232928+ayanagarwal2000@users.noreply.github.com>
2020-12-04 23:50:27 +0530

    Update segmenttree.java

--- java/segment_tree/segmenttree.java
+++ java/segment_tree/segmenttree.java
@@ -3,5 +3,21 @@ import java.util.*;
 
 public class segmenttree{
 
-    
+    private class node{
+        int data;
+        int startinterval;
+        int endinterval;
+        node left;
+        node right;
+    }
+
+    node root;
+
+    public segmenttree(int []arr){
+        this.constructSegmentTree(arr, 0,arr.length-1);
+    }
+
+    private node constructSegmentTree(int[] arr, int si, int ei){
+
+    }
 }
