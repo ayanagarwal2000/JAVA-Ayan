f0ddda9b7f0328bc0faa1396777e07fe2a06e29b
Ayan Agarwal <56232928+ayanagarwal2000@users.noreply.github.com>
2020-12-05 00:15:57 +0530

    Update segmenttree.java

--- java/segment_tree/segmenttree.java
+++ java/segment_tree/segmenttree.java
@@ -3,21 +3,39 @@ import java.util.*;
 
 public class segmenttree{
 
-    private class node{
+    private class Node{
         int data;
         int startinterval;
         int endinterval;
-        node left;
-        node right;
+        Node left;
+        Node right;
     }
 
-    node root;
+    Node root;
 
     public segmenttree(int []arr){
         this.root=constructTree(arr, 0,arr.length-1);
     }
 
-    private node constructTree(int[] arr, int si, int ei){
+    private Node constructTree(int[] arr, int si, int ei){
+        if(ei==si){
+            Node leafNode=new Node();
+            leafNode.startinterval=si;
+            leafNode.endinterval=ei;
+            leafNode.data=arr[si];
+            return leafNode;     
+        }
+
+        Node node=new Node();
+        node.startinterval=si;
+        node.endinterval=ei;
+
+        int mid=(si+ei)/2;
+        node.left=constructTree(arr, si, mid);
+        node.right=constructTree(arr, mid+1, ei);
+
+        node.data=node.left.data+ node.right.data;
+        return node;
 
     }
 }
